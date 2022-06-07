829f381459ecdcf4dc3bf16a9a3a70f4f1be8ebd
SCatella <samuel.catella@gmail.com>
2022-06-07 06:32:52 -0600

    replaced Class components in App.jsx and card-list.component.jsx to Functional Components

--- src/App.js
+++ src/App.js
@@ -1,56 +1,48 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import AppTitle from './components/app-title/app-title.component';
 import SearchBox from './components/search-box/search-box.component';
 import CardList from './components/card-list/card-list.component';
 import './App.css';
 
-class App extends Component {
+const App = () => {
 
-  constructor() {
-    super();
-    this.state = {
-      users: [],
-      searchField: ''
-    }
-  }
+  const [searchField, setSearchField] = useState('');
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState(users);
 
-  componentDidMount() {
+  useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return { users };
-      }))
-  }
+      .then((users) => setUsers(users));
+  }, []);
 
-  onSearchChange = (event) => {
-    const searchField = event.target.value.toLowerCase();
-    this.setState(() => {
-      return { searchField };
-    })
-  }
-
-  render() {
-    const { users, searchField } = this.state;
-    const { onSearchChange } = this;
-
-    const filteredUsers = users.filter((user) => {
+  useEffect(() => {
+    const newFilteredUsers = users.filter((user) => {
       return user.name.toLowerCase().includes(searchField);
     });
-    
 
-    return (
-      <div className="App">
-        <AppTitle />
-        <SearchBox
-          onChangeHandler={ onSearchChange }
-          className='search-box'
-          placeholder='search monsters'
-        />
-        <CardList users={ filteredUsers }/>
-      </div>
-    );
-  }
+    setFilteredUsers(newFilteredUsers);
+  }, [users, searchField]);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLowerCase();
+    setSearchField(searchFieldString);
+    }
+
+ 
+
+  return(
+    <div className="App">
+      <AppTitle />
+      <SearchBox
+        onChangeHandler={ onSearchChange }
+        className='search-box'
+        placeholder='search monsters'
+      />
+      <CardList users={ filteredUsers }/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,24 +1,16 @@
-import { Component } from 'react';
-
 import Card from '../card/card.component';
 import './card-list.styles.css'
 
-class CardList extends Component {
-  render() {
-    const { users } = this.props;
-
-    return (
-      <div className='card-list'>
-      {  //returns a card for each entry in the users array.
-        users.map((user) => {
-          return (
-            <Card user={ user }/>
-          ) 
-        })
-      }
-      </div>
-    )
-  }
-}
+const CardList = ({ users }) => (
+  <div className='card-list'>
+    {  //returns a card for each entry in the users array.
+      users.map((user) => {
+        return (
+          <Card user={user} />
+        )
+      })
+    }
+  </div>
+);
 
 export default CardList;
