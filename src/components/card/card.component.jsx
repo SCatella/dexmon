18a8f6f2a542d7787572a33f37b6d00d5ec4c699
SCatella <samuel.catella@gmail.com>
2022-06-07 06:37:34 -0600

    card.component.jsx converted to Fuctional Component from Class.

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,21 +1,17 @@
-import { Component } from 'react';
-
 import './card.styles.css'
 
-class Card extends Component {
-  render() {
-    const { name, email, id } = this.props.user;
-    return (
-      <div className='card' key={id}>
-        <img
-          className='card-image'
-          alt={`user: ${name}`}
-          src={`https://robohash.org/${id}?set2`} />
-        <h2>{ name }</h2>
-        <p>{ email }</p>
-      </div>
-    )
-  }
+const Card = ({ user }) => {
+  const { name, email, id } = user;
+  return (
+    <div className='card' key={id}>
+      <img
+        className='card-image'
+        alt={`user: ${name}`}
+        src={`https://robohash.org/${id}?set2`} />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
 }
 
 export default Card;
